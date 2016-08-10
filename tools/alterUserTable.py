e3bb1e1974583271ac337124df70a1dba069c54b
zhuyj17 <firmlyzhu@163.com>
2016-08-10 15:39:32 +0800

    Edit alterUserTable.py

--- tools/alterUserTable.py
+++ tools/alterUserTable.py
@@ -2,16 +2,23 @@ import sys
 sys.path.append("../src/")
 from model import db,User
 
+print("Query all users:")
 users = User.query.all()
-db.drop_all(bind='__all__')
 print(users)
-setattr(User,'beans',db.Column(db.Integer))
-db.create_all(bind='__all__')
+newusers = []
+print("Copy data to new users and set their beans to 10000...")
 for user in users:
     newuser = User(user.username,user.password,user.avatar,user.nickname,user.description,user.status,
                     user.e_mail,user.student_number,user.department,user.truename,user.tel,user.register_date,
                     user.user_group,user.auth_method)
-    newuser.beans = 1000
+    newuser.beans = 10000
+    newusers.append(newuser)
+print("Drop all table...")
+db.drop_all(bind='__all__')
+print("Create all tables with beans...")
+setattr(User,'beans',db.Column(db.Integer))
+db.create_all(bind='__all__')
+for newuser in newusers:
     db.session.add(newuser)
     db.session.commit()
-
+print("Update users table successfully!")
