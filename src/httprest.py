dcb85ef739653b1704d54b56e982d57a3d54bc24
zhuyj17 <firmlyzhu@163.com>
2016-09-26 09:33:12 +0800

    Fix little bugs.

--- src/httprest.py
+++ src/httprest.py
@@ -998,7 +998,7 @@ if __name__ == '__main__':
     master_collector.start()
     logger.info("master_collector started")
     G_applicationmgr = beansapplicationmgr.ApplicationMgr()
-    approvalrbt = beansapplicationmgr.ApprovalRobot(120)
+    approvalrbt = beansapplicationmgr.ApprovalRobot()
     approvalrbt.start()
     
     # server = http.server.HTTPServer((masterip, masterport), DockletHttpHandler)
