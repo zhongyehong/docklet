11dde65e3bcd4f5929c7de34df4816aa8cb84fd1
zhongyh16 <15289788668@163.com>
2017-05-08 16:11:47 +0800

    fix a bug of path in shell script

--- tools/nginx_config.sh
+++ tools/nginx_config.sh
@@ -5,14 +5,18 @@ NGINX_PORT=8080
 PROXY_PORT=8000
 WEB_PORT=8888
 NGINX_CONF=/etc/nginx
-DOCKLET_CONF=../conf
+
+toolsdir=${0%/*}
+DOCKLET_TOOLS=$(cd $toolsdir; pwd)
+DOCKLET_HOME=${DOCKLET_TOOLS%/*}
+DOCKLET_CONF=$DOCKLET_HOME/conf
 
 . $DOCKLET_CONF/docklet.conf
 
 NGINX_CONF=${NGINX_CONF}/sites-enabled
 
 echo "copy nginx_docklet.conf to nginx config path..."
-cp ../conf/nginx_docklet.conf ${NGINX_CONF}/
+cp $DOCKLET_CONF/nginx_docklet.conf ${NGINX_CONF}/
 sed -i "s/%MASTER_IP/${MASTER_IP}/g" ${NGINX_CONF}/nginx_docklet.conf
 sed -i "s/%NGINX_PORT/${NGINX_PORT}/g" ${NGINX_CONF}/nginx_docklet.conf
 if [ "${DISTRIBUTED_GATEWAY}" = "True" ]
