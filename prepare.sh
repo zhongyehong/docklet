162d86e75f05c9e374661463bdf83aec3136dd33
Junming Ma <majunming@outlook.com>
2017-10-24 18:36:29 +0800

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -63,7 +63,7 @@ echo ""
 
 echo "Before staring : you need a basefs image. "
 echo "basefs images are provided at: "
-echo "  http://unias.github.io/download"
+echo "  http://unias.github.io/docklet/download"
 echo "please download it to FS_PREFIX/local and then extract it using root. (defalut FS_PRERIX is /opt/docklet)"
 echo "you will get a directory structure like"
 echo "  /opt/docklet/local/basefs/etc "
