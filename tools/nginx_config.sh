ec8cedc57f05bf8ae6bd367ba4dbeef23973b0b1
zhuyj17 <firmlyzhu@163.com>
2018-12-12 23:26:51 +0800

    fix a bug in nginx_config.sh

--- tools/nginx_config.sh
+++ tools/nginx_config.sh
@@ -23,7 +23,7 @@ sed -i "s/%NGINX_PORT/${NGINX_PORT}/g" ${NGINX_CONF}/nginx_docklet.conf
 sed -i "s/%PROXY_PORT/${PROXY_PORT}/g" ${NGINX_CONF}/nginx_docklet.conf
 sed -i "s/%WEB_PORT/${WEB_PORT}/g" ${NGINX_CONF}/nginx_docklet.conf
 
-if [ "${NGINX_PORT}" != "80" ]
+if [ "${NGINX_PORT}" != "80" ] && [ "${NGINX_PORT}" != "443" ]
 then
   sed -i "s/\$host/\$host:\$server_port/g" ${NGINX_CONF}/nginx_docklet.conf
 fi
