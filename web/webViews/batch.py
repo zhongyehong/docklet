de21833f4cfcaf99b641f965caa142ccf301d81f
Firmlyzhu <firmlyzhu@163.com>
2019-03-31 16:09:29 +0800

    return error.html when errors occur in adding/stopping batch jobs

--- web/webViews/batch.py
+++ web/webViews/batch.py
@@ -32,10 +32,8 @@ class createBatchJobView(normalView):
         for master in masterips:
             images[master.split("@")[0]] = dockletRequest.post("/image/list/",{},master.split("@")[0]).get("images")
         logger.info(images)
-        if True:
-            return self.render(self.template_path, masterips=masterips, images=images)
-        else:
-            return self.error()
+        return self.render(self.template_path, masterips=masterips, images=images)
+
 
 class stateBatchJobView(normalView):
     template_path = "batch/batch_state.html"
@@ -49,28 +47,30 @@ class stateBatchJobView(normalView):
 
 class addBatchJobView(normalView):
     template_path = "batch/batch_list.html"
+    error_path = "error.html"
 
     @classmethod
     def post(self):
         masterip = self.masterip
         result = dockletRequest.post("/batch/job/add/", self.job_data, masterip)
-        #if result.get('success', None) == "true":
-        return redirect('/batch_jobs/')
-        #else:
-            #return self.error()
+        if result.get('success', None) == "true":
+            return redirect('/batch_jobs/')
+        else:
+            return self.render(self.error_path, message = result.get('message'))
 
 class stopBatchJobView(normalView):
     template_path = "batch/batch_list.html"
+    error_path = "error.html"
 
     @classmethod
     def get(self):
         masterip = self.masterip
         data = {'jobid':self.jobid}
         result = dockletRequest.post("/batch/job/stop/", data, masterip)
-        #if result.get('success', None) == "true":
-        return redirect('/batch_jobs/')
-        #else:
-            #return self.error()
+        if result.get('success', None) == "true":
+            return redirect('/batch_jobs/')
+        else:
+            return self.render(self.error_path, message = result.get('message'))
 
 class outputBatchJobView(normalView):
     template_path = "batch/batch_output.html"
